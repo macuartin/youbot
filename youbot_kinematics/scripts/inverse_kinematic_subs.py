e2691b0bd2547f4b1aa76586d739dbd9c4478ada
Miguel Cuartin <macuartin@gmail.com>
2020-03-28 22:54:16 -0500

    some fixes

--- youbot_kinematics/scripts/inverse_kinematic_subs.py
+++ youbot_kinematics/scripts/inverse_kinematic_subs.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 import rospy
+from std_msgs.msg import String
 
 
 def planner_listener():
-    return True
+    rospy.init_node('inverse_kinematic')
+    subscriber_topic = 'traj_planner_topic'
+    sub = rospy.Subscriber(subscriber_topic, String, printer)
+    rospy.spin()
+
+
+def printer(req):
+    print req
+
+
+if __name__ == '__main__':
+    planner_listener()
