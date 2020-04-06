35ee1d5514b18f621e19bada3d6b289c8168f9a0
Miguel Cuartin <macuartin@gmail.com>
2020-04-06 16:24:22 -0500

    change kinematic package for mechanics

--- youbot_mechanics/setup.py
+++ youbot_mechanics/setup.py
@@ -7,7 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['youbot_kinematics'],
+    packages=['youbot_mechanics'],
     package_dir={'': 'src'}
 )
 
